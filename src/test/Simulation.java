a5b550d5bf6795f9790214ae55054572a8014bf4
wl1390 <wl1390@nyu.edu>
2018-04-24 16:55:21 +0400

    add simple simulation

--- src/test/Simulation.java
+++ src/test/Simulation.java
@@ -1,8 +1,3 @@
-// clear README.md: run code, run test, get coverage reports
-// unit tests
-// user interface for the retail store
-
-
 package test;
 
 import model.Category;
@@ -10,67 +5,56 @@ import model.Item;
 import model.Store;
 import model.User;
 
-import java.util.ArrayList;
-
 public class Simulation{
 
 	public static void main(String[] args){
 	
-		Store store1 = new Store("Store 1");
-		Store store2 = new Store("Store 2");
-		User user1 = new User("User 1");
-		User user2 = new User("User 2");
-		User user3 = new User("User 3");
-		User user4 = new User("User 4");
-	
+		Store fruitstore = new Store("Fruit Store");
+		Store estore = new Store("Electronics Store");
+		User fruitlover = new User("I LOVE EATING FRUITS");
+		User techguy = new User("I LOVE TECH");
 
-		 
-		store1.putOnSale(new Item("apple",10,Category.GROCERY));
-		store1.putOnSale(new Item("phone",1000,Category.ELECTRONICS));
-		store1.putOnSale(new Item("car",10000,Category.OTHER));
+		System.out.println("Fruit store puts on sale apple and banana.");
+		fruitstore.putOnSale(new Item("apple",10,Category.GROCERY));
+		fruitstore.putOnSale(new Item("banana",10,Category.GROCERY));
 
-		store2.putOnSale(new Item("banana",100,Category.GROCERY));
-		store2.putOnSale(new Item("pear",100,Category.GROCERY));
-		store2.putOnSale(new Item("ball",100,Category.OTHER));
-
-	
-		 store1.employ(user2);
-		 store1.affiliate(user1);
-		 store2.addLoyalty(user2);
-
-
-		// store.display();
-
-
-
-		user1.enter(store1);
-		user1.addToBasket("car");
-		user1.addToBasket("phone");
-		user1.addToBasket("apple");
-
-		user1.enter(store2);
-		user1.addToBasket("ball");
+		System.out.println("Electronics store puts on sale lap and phone.");
+		estore.putOnSale(new Item("cellphone",1000,Category.ELECTRONICS));
+		estore.putOnSale(new Item("laptop",10000,Category.ELECTRONICS));
 		
-		user1.addToBasket("banana");
+		System.out.println("Fruitlover becomes loyalty in fruit store.");
+		fruitstore.addLoyalty(fruitlover);
 
-		user1.display();
-		System.out.println(user1.getSum());
+		System.out.println("Fruitlover enters fruit store.");
+		fruitlover.enter(fruitstore);
+
+		System.out.println("Fruitlover put 10 apples and 5 bananas into basket.");
+		estore.employ(techguy);
+		for (int i=0;i<10;i++){
+			fruitlover.addToBasket("apple");
+		}
+		for (int i=0;i<5;i++){
+			fruitlover.addToBasket("banana");
+		}		
+
+		System.out.println("Fruitlover should pay ( 10*10 + 5*10 ) - 5 = 145.");
+		System.out.println("Fruitlover checks out and pays "+ fruitlover.getSum()+".");
+
+		System.out.println();
+		System.out.println("Electronics Store hires techguy");
+		estore.employ(techguy);
+
+		System.out.println("Techguy enters Electronics store.");
+		techguy.enter(estore);
+
+		System.out.println("Techguy adds a laptop into basket");
+		techguy.addToBasket("laptop");
+
+		System.out.println("Techguy is an employee and should pay only 7000.");
+		System.out.println("Techguy pays "+techguy.getSum()+".");
 
 
-
-		user2.enter(store1);
-		user2.addToBasket("apple");
-		user2.enter(store2);
-		user2.addToBasket("banana");
-
-
-
-
-
-		user2.display();
-		System.out.println(user2.getSum());
-
-
+		System.out.println();
 		System.out.println("Complete");
 		
 	}
